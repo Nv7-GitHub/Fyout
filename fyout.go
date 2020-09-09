4a71526d68522a13a299baaa260148711ae79a55
Nv7 <junk4Nv7@gmail.com>
2020-09-09 10:29:55 -0700

    Added window settings

--- fyout.go
+++ fyout.go
@@ -4,15 +4,20 @@ import (
 	"os"
 
 	"fyne.io/fyne"
+	"fyne.io/fyne/theme"
 
 	"github.com/Nv7-Github/Fyout/widgets"
 )
 
 var w *fyne.Window
+var a *fyne.App
 
 // NewBuilder creates the builder UI
-func NewBuilder(path string, window *fyne.Window) {
-	w = window
+func NewBuilder(path string, app *fyne.App) {
+	a = app
+	win := (*app).NewWindow("Fyout Layout Builder")
+	win.Resize(fyne.NewSize(800, 600))
+	w = &win
 	_, err := os.Stat(path)
 	exists := os.IsExist(err)
 	if !exists {
@@ -23,4 +28,24 @@ func NewBuilder(path string, window *fyne.Window) {
 		Widgets.Init(w)
 		Widgets.UpdateUI()
 	}
+	MainMenu(w)
+	(*w).Show()
+}
+
+// MainMenu creates the menu for the builder
+func MainMenu(win *fyne.Window) {
+	themeMenu := fyne.NewMenuItem("Theme", nil)
+	themeMenu.ChildMenu = fyne.NewMenu("",
+		fyne.NewMenuItem("Dark Theme", func() {
+			(*a).Settings().SetTheme(theme.DarkTheme())
+			Widgets.UpdateUI()
+		}),
+		fyne.NewMenuItem("Light Theme", func() {
+			(*a).Settings().SetTheme(theme.LightTheme())
+			Widgets.UpdateUI()
+		}),
+	)
+	settingsMenu := fyne.NewMenu("Settings", themeMenu)
+	mainMenu := fyne.NewMainMenu(settingsMenu)
+	(*w).SetMainMenu(mainMenu)
 }
